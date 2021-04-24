adbd6a0f56f9c23ea5724e2c7362bb700b135e2f
조우준 <jowoojun77@gmail.com>
2021-04-24 22:52:08 +0900

    feat: 이미지 확대 overlay 기능 추가

---
+++ front/src/ImagesZoom/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import Slick from 'react-slick';
+import PropTypes from 'prop-types';
+import { Global } from '@emotion/react'
+
+import { Overlay, Header, CloseBtn, SlickWrapper, ImgWrapper, Indicator, GlobalStyle } from './styles';
+
+const ImagesZoom = ({ images, onClose }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  return (
+    <Overlay>
+      <Global styles={GlobalStyle} />
+      <Header>
+        <h1>상세 이미지</h1>
+        <CloseBtn onClick={onClose} />
+      </Header>
+      <SlickWrapper>
+        <div>
+          <Slick
+            initialSlide={0}
+            beforeChange={(slide, newSlide) => setCurrentSlide(newSlide)}
+            infinite
+            arrows={false}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            {images.map((v) => (
+              <ImgWrapper key={v.src}>
+                <img src={v.src} alt={v.src} />
+              </ImgWrapper>
+            ))}
+          </Slick>
+          <Indicator>
+            <div>
+              {currentSlide + 1}
+              {' '}
+              /
+              {' '}
+              {images.length}
+            </div>
+          </Indicator>
+        </div>
+      </SlickWrapper>
+    </Overlay>
+  );
+};
+
+ImagesZoom.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.shape({
+    src: PropTypes.string,
+  })).isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ImagesZoom;
--- front/src/PostImages.js
+++ front/src/PostImages.js
@@ -1,18 +1,30 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import styled from '@emotion/styled'
+import React, { useCallback, useState } from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled"
+import { PlusOutlined } from "@ant-design/icons"
 
-import { PlusOutlined } from "@ant-design/icons";
+import ImagesZoom from "./ImagesZoom"
 
 const ImageContainer = styled.div`
   margin-top: 10px;
 `
 
-const PostImages = ({images}) => {
+const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false)
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true)
+  }, [])
+
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false)
+  }, [])
+
   if(images.length === 1){
     return(
       <ImageContainer>
-        <img role="presentation" src={images[0].src} alt={images[0].src} />
+        <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom}/>
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </ImageContainer>
     )
   }
@@ -20,25 +32,31 @@ const PostImages = ({images}) => {
     return(
       <ImageContainer>
         <div>
-          <img role="presentation" style={{width: "50%"}} src={images[0].src} alt={images[0].src} />
-          <img role="presentation" style={{width: "50%"}} src={images[1].src} alt={images[1].src} />
+          <img role="presentation" style={{width: "50%"}} src={images[0].src} alt={images[0].src} onClick={onZoom}/>
+          <img role="presentation" style={{width: "50%"}} src={images[1].src} alt={images[1].src} onClick={onZoom}/>
         </div>
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </ImageContainer>
     )
   }
   return(
     <ImageContainer>
-      <div>
-        <img role="presentation" style={{width: "50%"}} src={images[0].src} alt={images[0].src} />
+      <div style={{display:"flex", flexDirection:"row"}}>
+        <div style={{flex: "1"}} >
+          <img role="presentation" style={{width: "100%"}} src={images[0].src} alt={images[0].src} onClick={onZoom}/>
+        </div>
         <div
           role="presentation"
-          style={{display:"inline-block", width: "50%", textAlign: "center", verticalAlign: "center"}}
+          style={{display: "flex", flex: "1", width: "100%", textAlign: "center", justifyContent: "center", alignItems: "center", border: "dashed 0.5px"}}
+          onClick={onZoom}
         >
-          <PlusOutlined />
-          <br />
-          {images.length - 1}
-          개의 사진 더보기 
+          <div>
+            <PlusOutlined />
+            <br />
+            {images.length - 1}개의 사진 더보기 
+          </div>
         </div>
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
       </div>
     </ImageContainer>
   )
@@ -48,6 +66,6 @@ PostImages.propTypes = {
   images: PropTypes.arrayOf(PropTypes.shape({
     src: PropTypes.string,
   })).isRequired,
-};
+}
 
-export default PostImages;
+export default PostImages
