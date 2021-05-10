22970daeeadfb3228e176652a1e6008f97a68bcf
조우준 <jowoojun77@gmail.com>
2021-05-11 00:09:31 +0900

    feat_back: 회원가입 라우터 개발

--- back/app.js
+++ back/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const db = require('./models');
-
+const cors = require('cors')
 const app = express();
 
 const userRouter = require('./routes/user');
 const postRouter = require('./routes/post');
 
-app.use('/api', userRouter);
+app.use(cors({ // Access-Control-Allow-Origin 에러
+  origin: '*', // 모든 url 접근 허락
+  credentials: false,
+}))
+app.use(express.json()); // front에서 데이터를 json 형식으로 받음
+app.use(express.urlencoded({ extended: true })); // form 태그의 submit 데이터를 전송받음
+
+app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
 
 // db연결
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router();
+const bcrypt = require('bcrypt')
+const { User } = require('../models')
 
 router.post('/login', (req, res) => {
   res.send('Hello, login!')
@@ -9,8 +11,28 @@ router.post('/logout', (req, res) => {
   res.send('Hello, logout!')
 })
 
-router.post('/signUp', (req, res) => {
-  res.send('Hello, signUp!')
+router.post('/signUp', async (req, res, next) => {
+  try{
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    if(exUser){
+      return res.status(403).send('이미 사용중인 이메일입니다.')
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12)
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nick,
+      password: hashedPassword,
+    })
+    
+    res.status(201).send('success')
+  }catch(err) {
+    console.error(err)
+    next(err)
+  }
 })
 
 router.post('/follow', (req, res) => {
