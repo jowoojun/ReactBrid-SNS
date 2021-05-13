45c7dc13c25f10f0a4cb0a65e8b04960293453ad
조우준 <jowoojun77@gmail.com>
2021-05-13 18:36:16 +0900

    feat_back: 새로고침 후에도 로그인이 풀리지 않도록 메인화면이 갱신될 때마다 사용자 정보를 보내주도록 수정

--- back/routes/user.js
+++ back/routes/user.js
@@ -6,6 +6,37 @@ const passport = require('passport');
 const { User, Post } = require('../models');
 const { needLogin, needNotLogin } = require('./middlewares')
 
+router.get('/', async (req, res, next) => {
+  try{
+    if(req.user){
+      const safeUser = await User.findOne({
+        where: { id: req.user.id },
+        attributes: {
+          exclude: ['password']
+        },
+        include: [{
+          model: Post,
+          attributes: ['id'],
+        }, {
+          model: User,
+          as: 'Followings',
+          attributes: ['id'],
+        }, {
+          model: User,
+          as: 'Followers',
+          attributes: ['id'],
+        }]
+      })
+      res.status(200).json(safeUser);
+    } else {
+      res.status(200).json(null);
+    }
+  } catch(err) {
+    console.error(err)
+    next(err);
+  }
+})
+
 router.post('/login', needNotLogin, (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
     if(err){
@@ -27,12 +58,15 @@ router.post('/login', needNotLogin, (req, res, next) => {
         },
         include: [{
           model: Post,
+          attributes: ['id'],
         }, {
           model: User,
           as: 'Followings',
+          attributes: ['id'],
         }, {
           model: User,
           as: 'Followers',
+          attributes: ['id'],
         }]
       })
       return res.status(200).json(safeUser); // 200: 성공
