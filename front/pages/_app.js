a9c527e2ac1c9aa3104bcff1daaff4bb9ddcf408
조우준 <jowoojun77@gmail.com>
2021-05-18 14:20:07 +0900

    fix: 불필요한 패키지 삭제

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
-import withReduxSaga from 'next-redux-saga';
 
 import wrapper from '../store/configureStore';
 
@@ -21,4 +20,4 @@ App.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default wrapper.withRedux(withReduxSaga(App));
+export default wrapper.withRedux(App);
